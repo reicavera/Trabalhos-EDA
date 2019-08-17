aff51d5dbdf83e4571a1d77050af056f76490a56
reicavera <49381208+reicavera@users.noreply.github.com>
2019-08-17 15:23:32 -0300

    Update pilha.c

--- pilha.c
+++ pilha.c
@@ -9,24 +9,26 @@ do proximo registro na pilha.*/
 };
 typedef struct no reg;
 struct pilha{
-/* Criando o struct "pilha" que guarda o endereco do ultimo registro adicionado
-    referente ao guiche escolhido.*/
+/* Criando o struct "pilha" que guarda o endereço do último registro adicionado
+e o número de registros referente ao guiche escolhido.*/
     reg* topo;
+    long long cont;
 };
 typedef struct pilha guiche;
 guiche* create_guiche(){
-/*Criando efetivamente um guiche atraves de ums variavel auxiliar.O endereco da
-memoria criada sera enviado para aux e o topo do cuiche nao apontara para nada.
-Essa funcao retorna o endereco criado.*/
+/*Criando efetivamente um guiche através de uma variável auxiliar.O endereço da
+memória criada será enviado para aux e o topo do guiche não apontará para nada.
+Essa funcão retorna o endereço criado.*/
     guiche* aux;
     aux=malloc(sizeof(guiche));
     aux->topo=NULL;
+    aux->cont=0;
     return aux;
 }
 reg* create_reg(guiche* gn){
-/*Criando efetivamente um registro atraves de ums variavel auxiliar.O endereco da
-memoria criada sera enviado para aux,que fara com que a nova memoria aponte para
-o ropo da pilha.Essa funcao retorna o endereco da memoria criada.*/
+/*Criando efetivamente um registro através de uma variável auxiliar.O endereço da
+memória criada será enviado para aux,que fará com que a nova memória aponte para
+o ropo da pilha.Essa função retorna o endereço da memória criada.*/
     reg* aux;
     aux=malloc(sizeof(reg));
     aux->prox=gn->topo;
@@ -53,8 +55,8 @@ void delete_all(guiche* gn){
     free(gn);
 }
 int main(){
-/*declarando as variaveis que iremos precisar:N indicara a quantidade de registros,
-v sera o vetor que apontara para cada guiche e o resto para o scanf dos dados do
+/*declarando as variáveis que iremos precisar:N indicará a quantidade de registros,
+v será o vetor que apontará para cada guiche e o resto para o scanf dos dados do
 registro.*/
     long long n,scpfc,scpft,svalor;
     char sop;
@@ -62,7 +64,7 @@ registro.*/
 /*Criando os 3 guiches e fazendo v apontar para cada um dos guiches*/
     for(int i=0;i<3;i++)
         v[i]=create_guiche();
-/*Lendo as entradas do programa.Sera criado N registros com os dados que o usuario entrar*/    
+/*Lendo as entradas do programa.Será criado N registros com os dados que o usuário entrar*/
 	scanf("%lli",&n);
     for(long long i=0;i<n;i++){
         scanf("%lli %lli %c %lli",&scpfc,&scpft,&sop,&svalor);
@@ -71,11 +73,12 @@ registro.*/
         (v[i%3]->topo)->cpft=scpft;
         (v[i%3]->topo)->op=sop;
         (v[i%3]->topo)->valor=svalor;
+        v[i%3]->cont++;
     }
-/*Saida do programa.Mostra os registros de cada guiche*/
+/*Saída do programa.Mostra os registros de cada guiche*/
     printf("-:| RELATÓRIO PARCIAL |:-\n3\n");
     for(int i=0;i<3;i++){
-        printf("guiche %d\n",i+1);
+        printf("guiche %d: %lli\n",i+1,v[i]->cont);
         show_all(v[i]);
     }
 /*Deleta todos os guiches,assim como os dados que eles continham*/
@@ -83,3 +86,4 @@ registro.*/
         delete_all(v[i]);
 return 0;
 }
+
