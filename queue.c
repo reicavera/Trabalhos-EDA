8ff39f97ac384a0d8a398d21417a0b6894a17458
reicavera <49381208+reicavera@users.noreply.github.com>
2019-10-23 00:41:35 -0300

    Create queue.c

---
+++ queue.c
@@ -0,0 +1,42 @@
+#include<stdlib.h>
+#include "queue.h"
+queue* create_queue(long long n){
+     //Cria uma fila circular estática. Seus elementos são nós padrões
+    queue* aux;
+    aux=malloc(sizeof(queue));
+    aux->data=malloc(sizeof(node_p)*n);
+    aux->max_size=n;
+    aux->size=0;
+    aux->front=0;
+    aux->bottom=0;
+    return aux;
+}
+
+void add_queue(queue* f,long long scpfc,long long scpft,char sop,long long svalor){
+    //Coloca os valores do cpf cliente,cpf terceiro,valor e a operação no fundo da fila,
+    //muda o novo fundo para o próximo elemento e incrementa em 1 o indicador do tamanho
+    //atual da fila.
+    f->data[f->bottom].cpfc=scpfc;
+    f->data[f->bottom].cpft=scpft;
+    f->data[f->bottom].op=sop;
+    f->data[f->bottom].valor=svalor;
+    f->bottom=(f->bottom+1)%f->max_size;
+    f->size++;
+}
+node_p* remove_queue(queue* f){
+    //Remove elemento da fila. Retorna o nó padrão removido.
+    node_p* aux;
+    aux=create_node_p();
+    aux->cpfc=f->data[f->front].cpfc;
+    aux->cpft=f->data[f->front].cpft;
+    aux->op=f->data[f->front].op;
+    aux->valor=f->data[f->front].valor;
+    f->front=(f->front+1)%f->max_size;
+    f->size--;
+    return aux;
+}
+void destroy_queue(queue* f){
+     //Desaloca a fila e seus elementos. Não retorna nada.
+    free(f->data);
+    free(f);
+}
