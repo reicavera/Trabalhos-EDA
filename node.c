974323722d9d3b3989d71ca3f7e85f3f286c8c60
reicavera <49381208+reicavera@users.noreply.github.com>
2019-10-28 20:26:08 -0300

    Update node.c

--- node.c
+++ node.c
@@ -1,7 +1,7 @@
 #include<stdlib.h>
 #include "node.h"
 node_p* create_node_p(){
-    //Um auxiliar aloca espaço para um Nó padrão e retorna um endereço para o mesmo.
+    //Um auxiliar aloca espaço para um nó padrão e retorna um endereço para o mesmo.
     node_p* aux=NULL;
     aux=malloc(sizeof(node_p));
     if(aux==NULL)
@@ -9,19 +9,23 @@ node_p* create_node_p(){
     return aux;
 }
 long long node_p_display_cpfc(node_p* p){
+    //dado um nó padrão,retorna o cpf do cliente.
     return p->cpfc;
 }
 long long node_p_display_cpft(node_p* p){
+    //dado um nó padrão,retorna o cpf de terceiros.
     return p->cpft;
 }
 long long node_p_display_valor(node_p* p){
+    //dado um nó padrão,retorna o valor da operação.
     return p->valor;
 }
 char node_p_display_op(node_p* p){
+    //dado um nó padrão,retorna o tipo de operação realizada.
     return p->op;
 }
 void destroy_node_p(node_p* n){
-    //Recebe o endereço de um Nó padrão e desaloca o espaço ocupado por ele.
+    //Recebe o endereço de um nó padrão e desaloca o espaço ocupado por ele.
     free(n);
     n=NULL;
 }
@@ -36,16 +40,19 @@ node_l* create_node_l(){
     return n;
 }
 long long node_l_display_cpf(node_l* n){
+    //dado um nó de lista,retorna o cpf.
     return n->cpf;
 }
 long long node_l_display_nop(node_l* n){
+    //dado um nó de lista,retorna o numero de operações.
     return n->nop;
 }
 long long node_l_display_balance(node_l* n){
+    //dado um nó de lista,retorna o saldo.
     return n->balance;
 }
 void destroy_node_l(node_l* n){
-     //Desaloca o espaço ocupado por esse Nó na memória.
+     //Desaloca o espaço ocupado por esse nó na memória.
     free(n);
     n=NULL;
 }
