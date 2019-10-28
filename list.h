d580fb06fd75ed980f1fe73f38218d6b32ad7710
reicavera <49381208+reicavera@users.noreply.github.com>
2019-10-28 01:14:29 -0300

    Update list.h

--- list.h
+++ list.h
@@ -9,10 +9,9 @@ struct list{
     node_l* first;
     long long counter;
 };
-typedef struct list list;
 list* create_list();
-void search_list(long long valor,long long cpf,list* l);
-node_p* add_list(list* l,stack* s);
+long long list_size(list* l);
+void add_list(list* l,node_p* n);
 node_l* remove_list_first(list* l);
 void destroy_list(list* l);
 
