4a16bffb562159226295e5f154333368349b6595
reicavera <49381208+reicavera@users.noreply.github.com>
2019-10-28 15:32:38 -0300

    Update node.h

--- node.h
+++ node.h
@@ -13,6 +13,10 @@ struct node_p{
 };
 typedef struct node_p node_p;
 node_p* create_node_p();
+long long node_p_display_cpfc(node_p* p);
+long long node_p_display_cpft(node_p* p);
+long long node_p_display_valor(node_p* p);
+char node_p_display_op(node_p* p);
 void destroy_node_p(node_p* n);
 struct node_l{
     //Define o registro de dados para impressão do relatório final como Nó de lista.
@@ -22,6 +26,9 @@ struct node_l{
 };
 typedef struct node_l node_l;
 node_l* create_node_l();
+long long node_l_display_cpf(node_l* n);
+long long node_l_display_nop(node_l* n);
+long long node_l_display_balance(node_l* n);
 void destroy_node_l(node_l* n);
 
 #endif
