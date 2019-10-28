8f65e3fb8bd314555567b7409d8168cf6f627a9f
reicavera <49381208+reicavera@users.noreply.github.com>
2019-10-28 20:43:50 -0300

    Update queue.h

--- queue.h
+++ queue.h
@@ -11,9 +11,10 @@ struct queue{
     long long max_size,size,front,bottom;
 };
 typedef struct queue queue;
-queue* create_queue(long long n);
-void add_queue(queue* f,long long scpfc,long long scpft,char sop,long long svalor);
-node_p* remove_queue(queue* f);
-void destroy_queue(queue* f);
+queue* create_queue(long long n);//cria uma fila com tamanho n.
+void add_queue(queue* f,long long scpfc,long long scpft,char sop,long long svalor);//Adiciona no fundo da fila
+//as informações de cpf do cliente,cpf de terceiros,valor e o tipo de operação.
+node_p* remove_queue(queue* f);//Remove a frente da fila e retorna um nó padrão com os dados da casa removida. 
+void destroy_queue(queue* f);//Desaloca o vetor com os dados e a fila em si. 
 
 #endif
