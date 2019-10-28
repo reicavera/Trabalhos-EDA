8fe85326c99677c40565d359878189005f5676ae
reicavera <49381208+reicavera@users.noreply.github.com>
2019-10-28 20:58:07 -0300

    Update list.h

--- list.h
+++ list.h
@@ -11,10 +11,12 @@ struct list{
     long long counter;
 };
 typedef struct list list;
-list* create_list();
-long long list_size(list* l);
-void add_list(list* l,node_p* n);
-node_l* remove_list_first(list* l);
-void destroy_list(list* l);
+list* create_list();//cria uma lista e a reorna.
+long long list_size(list* l);//retorna o numero de elementos na lista.
+void add_list(list* l,node_p* n);//Faz todo o controle de dados a partir do tipo de operação
+//e adiciona um novo dado ordenando de acordo com o cpf caso ainda não exista algum dos cpfs e altera o 
+//saldo e incrementa o numero de operações caso o algum dos cpfs ja esteja na lista.
+node_l* remove_list_first(list* l);//Retira o primeiro elemento da lista e o retorna.
+void destroy_list(list* l);//Destroi a lista e todos os elementos dela.
 
 #endif // LIST
