c30872b00fbd1b4a0591cee615007f77a0c5dd68
reicavera <49381208+reicavera@users.noreply.github.com>
2019-10-28 01:15:41 -0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -2,16 +2,19 @@
 #include "queue.h"
 queue* create_queue(long long n){
      //Cria uma fila circular estática. Seus elementos são nós padrões
-    queue* aux;
+    queue* aux=NULL;
     aux=malloc(sizeof(queue));
+    if(aux==NULL)
+        return NULL;
     aux->data=malloc(sizeof(node_p)*n);
+    if(aux->data==NULL)
+        return NULL;
     aux->max_size=n;
     aux->size=0;
     aux->front=0;
     aux->bottom=0;
     return aux;
 }
-
 void add_queue(queue* f,long long scpfc,long long scpft,char sop,long long svalor){
     //Coloca os valores do cpf cliente,cpf terceiro,valor e a operação no fundo da fila,
     //muda o novo fundo para o próximo elemento e incrementa em 1 o indicador do tamanho
@@ -38,5 +41,7 @@ node_p* remove_queue(queue* f){
 void destroy_queue(queue* f){
      //Desaloca a fila e seus elementos. Não retorna nada.
     free(f->data);
+    f->data=NULL;
     free(f);
+    f=NULL;
 }
